a26e41c5d3bc0e7990f5db16f197d6f697bc69f3
折乙 <zheyi563@gmail.com>
2025-03-28 11:37:22 +0800

    feat(NewVoice,PPTCreation): 收藏弹窗,更多声音调节

--- src/views/NewVoice.vue
+++ src/views/NewVoice.vue
@@ -64,10 +64,47 @@
            <div style="display: flex;align-items: center;">
             <img src="@/assets/icons/voice.png" style="width: 1.5em;height: 1.1em;" >
           </div>
-           <el-button type="primary" style="width: 140px;">
+           <el-button type="primary" style="width: 140px;" @click="isShowMore=!isShowMore">
             <img src="@/assets/icons/：.png" style="width: 0.8em;height: 1.3em;" >
             更多声音调节选项
           </el-button>
+          <div class="more-voice" v-if="isShowMore">
+            <el-dropdown @command="SpeedSelect">
+              <el-button>
+                倍速
+                <span v-if="Speed!==''">:{{Speed}}</span>
+                <el-icon class="el-icon--right" v-else><arrow-up /></el-icon>
+              </el-button>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item command="0.4"><span style="width: 100%;;text-align: center;">0.4</span></el-dropdown-item>
+                  <el-dropdown-item command="0.6"><span style="width: 100%;;text-align: center;">0.6</span></el-dropdown-item>
+                  <el-dropdown-item command="0.8"><span style="width: 100%;;text-align: center;">0.8</span></el-dropdown-item>
+                  <el-dropdown-item command=""><span style="width: 100%;;text-align: center;">1.0</span></el-dropdown-item>
+                  <el-dropdown-item command="1.2"><span style="width: 100%;;text-align: center;">1.2</span></el-dropdown-item>
+                  <el-dropdown-item command="1.4"><span style="width: 100%;;text-align: center;">1.4</span></el-dropdown-item>
+                  <el-dropdown-item command="1.6"><span style="width: 100%;;text-align: center;">1.6</span></el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+            <el-dropdown @command="VoiceSelect">
+              <el-button>
+                音调<span v-if="Voice!==''">:{{Voice}}</span>
+                <el-icon class="el-icon--right" v-else><arrow-up /></el-icon>
+              </el-button>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item command="+50%"><span style="width: 100%;;text-align: center;">升高50%</span></el-dropdown-item>
+                  <el-dropdown-item command="+25%"><span style="width: 100%;;text-align: center;">升高25%</span></el-dropdown-item>
+                  <el-dropdown-item command="+10%"><span style="width: 100%;;text-align: center;">升高10%</span></el-dropdown-item>
+                  <el-dropdown-item command=""><span style="width: 100%;;text-align: center;">原声调</span></el-dropdown-item>
+                  <el-dropdown-item command="-10%"><span style="width: 100%;;text-align: center;">降低10%</span></el-dropdown-item>
+                  <el-dropdown-item command="-25%"><span style="width: 100%;;text-align: center;">降低25%</span></el-dropdown-item>
+                  <el-dropdown-item command="-50%"><span style="width: 100%;;text-align: center;">降低50%</span></el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </div>
         </div>
         <div class="card-block">
           <el-button
@@ -78,6 +115,43 @@
           >
           收藏声音
         </el-button>
+        <el-dialog
+          v-model="dialogVisible"
+          width="50%"
+          align-center
+          :before-close="handleClose"
+        >
+          <el-form
+            ref="formRef"
+            :model="form"
+            :rules="rules"
+            label-width="80px"
+            status-icon
+          >
+            <el-form-item label="命名" prop="name">
+              <el-input
+                v-model="form.name"
+                placeholder="请输入"
+                clearable
+              ></el-input>
+            </el-form-item>
+
+            <el-form-item label="分组" prop="group">
+              <el-input
+                v-model="form.group"
+                placeholder="默认收藏夹"
+                clearable
+              ></el-input>
+            </el-form-item>
+          </el-form>
+
+          <template #footer>
+            <span class="dialog-footer">
+              <el-button @click="handleClose">取消</el-button>
+              <el-button type="primary" @click="submitForm">确定</el-button>
+            </span>
+          </template>
+        </el-dialog>
         </div>
     </Card>
     </div>
@@ -88,7 +162,7 @@ import LayOut from '@/components/layouts/LayOut.vue';
 import Card from '@/components/card/CarD.vue';
 import DropDownSelector from '@/components/dropDownSelecter/dropDownSelector.vue';
 import { lanConfig } from '@/assets/constants';
-import { ref } from 'vue';
+import { ref ,reactive } from 'vue';
 import { ElMessage, ElMessageBox } from 'element-plus'
 
 const isPlaying = ref(false);
@@ -97,27 +171,67 @@ function play() {
   isPlaying.value = !isPlaying.value;
 }
 
+const isShowMore = ref(false);
+const Speed = ref("");
+const Voice = ref("");
+
+function SpeedSelect(command: string) {
+  Speed.value = command;
+}
+function VoiceSelect(command: string) {
+  Voice.value = command;
+}
 function Collect() {
-  ElMessageBox.prompt('Please input your e-mail', 'Tip', {
-    confirmButtonText: 'OK',
-    cancelButtonText: 'Cancel',
-    inputPattern:
-      /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
-    inputErrorMessage: 'Invalid Email',
-  })
-    .then(({ value }) => {
-      ElMessage({
-        type: 'success',
-        message: `Your email is:${value}`,
-      })
+  dialogVisible.value = true;
+}
+const dialogVisible = ref(false);
+const formRef = ref();
+
+const  form = reactive({
+  name: "",
+  group: "",
+});
+
+const  rules = reactive({
+  name: [
+    { required: true, message: "请输入命名", trigger: "blur" },
+    { min: 1, max: 10, message: "长度在 1 到 10 个字符", trigger: "blur" },
+  ],
+  group: [
+    { required: true, message: "请输入分组", trigger: "blur" },
+    { min: 1, max: 10, message: "长度在 1 到 10 个字符", trigger: "blur" },
+  ],
+});
+
+
+const  handleClose = () => {
+  dialogVisible.value = false;
+  formRef.value.resetFields();
+};
+
+const  submitForm = () => {
+formRef.value.validate((valid: boolean) => {
+  if (valid) {
+    ElMessageBox.confirm("确定要添加收藏吗？", "提示", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: "info",
+    })
+    .then(() => {
+      // 提交表单逻辑
+      ElMessage.success("收藏成功！");
+      handleClose();
     })
     .catch(() => {
-      ElMessage({
-        type: 'info',
-        message: 'Input canceled',
-      })
-    })
-}
+      ElMessage.info("已取消收藏");
+    });
+    } else {
+        ElMessage.error("表单验证失败，请检查输入内容！");
+        return false;
+      }
+  });
+};
+
 </script>
 <style lang="scss" scoped>
 .card-container {
@@ -135,6 +249,14 @@ function Collect() {
         font-size: 16px;
         line-height: 35px;
         color: #4B5563;
+      }
+
+      .more-voice {
+        display: flex;
+        align-items: center;
+        gap: 25px;
+        width: 40%;
+        height: 30px;
       }
     }
   }
